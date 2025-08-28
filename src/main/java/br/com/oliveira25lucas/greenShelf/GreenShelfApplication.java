14aa1689b07368ec6ee81ce7e5a6dd1ff0526599
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-08-27 22:26:27 -0300

    feat: add Spring features

---
+++ src/main/java/br/com/oliveira25lucas/greenShelf/GreenShelfApplication.java
@@ -0,0 +1,13 @@
+package br.com.oliveira25lucas.greenShelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oliveira25lucas/greenShelf/GreenShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oliveira25lucas.greenShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
