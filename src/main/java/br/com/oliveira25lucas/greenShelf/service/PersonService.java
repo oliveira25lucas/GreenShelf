1cf01e58ad9f091705e0fde49ab30281c98e9298
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-09-04 19:48:08 -0300

    feat: fix logs

--- src/main/java/br/com/oliveira25lucas/greenShelf/service/PersonService.java
+++ src/main/java/br/com/oliveira25lucas/greenShelf/service/PersonService.java
@@ -3,19 +3,20 @@ package br.com.oliveira25lucas.greenShelf.service;
 import br.com.oliveira25lucas.greenShelf.exception.ResourceNotFoundException;
 import br.com.oliveira25lucas.greenShelf.model.Person;
 import br.com.oliveira25lucas.greenShelf.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonService.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonService.class.getName());
 
     @Autowired
     PersonRepository repository;
